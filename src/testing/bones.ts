7be258fa3ee3b05b2d100096bd580f338f8a4606
Jacek Kopecky <jacek@jacek.cz>
2026-03-29 14:37:43 +0100

    have more players side by side

--- src/testing/bones.ts
+++ src/testing/bones.ts
@@ -4,22 +4,22 @@ import { timer } from '../run/three/main';
 import { Legs } from './legs';
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 200);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const scene = new THREE.Scene();
 
 function initScene() {
   scene.background = new THREE.Color(0xaaccee);
 
   const centerY = 60;
-  camera.position.set(50, centerY + 20, 70);
+  camera.position.set(0, centerY + 20, -200);
 
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
   const controls = new OrbitControls(camera, renderer.domElement);
-  controls.target = new THREE.Vector3(0, centerY, 0);
-  controls.rotateLeft(Math.PI / 2);
+  controls.target = new THREE.Vector3(40, centerY, 0);
+  controls.rotateLeft((Math.PI / 180) * 225);
   controls.update();
 
   // lights
@@ -44,7 +44,7 @@ function initScene() {
   initBones();
 }
 
-let legs: Legs;
+let players: Legs[];
 
 function initBones() {
   const material = new THREE.MeshLambertMaterial({
@@ -54,16 +54,27 @@ function initBones() {
     flatShading: true,
   });
 
-  legs = new Legs({ length: 40, radius: 6 }, 25, material);
+  players = [
+    new Legs({ length: 40, radius: 6 }, 25, material),
+    new Legs({ length: 40, radius: 6 }, 25, material),
+    new Legs({ length: 40, radius: 6 }, 25, material),
+    new Legs({ length: 40, radius: 6 }, 25, material),
+  ];
 
-  scene.add(legs.object);
+  for (let i = 0; i < players.length; i += 1) {
+    const legs = players[i]!;
+    scene.add(legs.object);
+    legs.object.position.x += i * 30;
+  }
 }
 
 function render(ms?: number) {
   requestAnimationFrame(render);
 
   timer.update(ms);
-  legs.update(timer.getDelta());
+  for (const legs of players) {
+    legs.update(timer.getDelta());
+  }
   renderer.render(scene, camera);
 }
 
@@ -72,8 +83,10 @@ let moving = false;
 document.addEventListener('keydown', (e) => {
   if (e.key === ' ') {
     moving = !moving;
-    if (moving) legs.startWalking();
-    else legs.stopWalking();
+    for (const legs of players) {
+      if (moving) legs.startWalking();
+      else legs.stopWalking();
+    }
   }
 });
 
