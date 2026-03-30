6a9963fd99fe9e1386119021bdfd7674183e4394
Jacek Kopecky <jacek@jacek.cz>
2026-03-30 19:44:48 +0100

    log fps into a dedicated element if present

--- src/common/log.ts
+++ src/common/log.ts
@@ -1,4 +1,5 @@
 const logEl = document.querySelector('#log');
+const fpsEl = document.querySelector('#fps');
 
 export function log(str: string) {
   if (!logEl) return;
@@ -12,12 +13,16 @@ let lastTimeMs: number | undefined;
 let framesThisSecond = 0;
 
 export function logFps(ms?: number, prefix = '') {
-  if (!logEl) return;
+  if (!logEl && !fpsEl) return;
 
   if (ms != null && lastTimeMs != null) {
     framesThisSecond += 1;
     if (lastTimeMs % 1000 > ms % 1000) {
-      log(`${prefix}${framesThisSecond}`);
+      if (fpsEl) {
+        fpsEl.textContent = String(framesThisSecond);
+      } else {
+        log(`${prefix}${framesThisSecond}`);
+      }
       framesThisSecond = 0;
     }
   }
